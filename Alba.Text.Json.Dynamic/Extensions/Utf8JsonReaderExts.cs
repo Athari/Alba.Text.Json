c6d5bca66e7e0e58d178d09d87a97fde6e394c62
Athari <athari.p@gmail.com>
2025-12-09 16:01:11 +0300

    Added Utf8JsonReaderExts and Utf8JsonWriterExts for Newtonsoft.Json-like converters support.

---
+++ Alba.Text.Json.Dynamic/Extensions/Utf8JsonReaderExts.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json;
+
+namespace Alba.Text.Json.Dynamic.Extensions;
+
+public static class Utf8JsonReaderExts
+{
+    extension(ref Utf8JsonReader @this)
+    {
+        public object? GetValue(JNodeOptions options) => JsonElement.ParseValue(ref @this).ToValue(options);
+    }
+}
---
+++ Alba.Text.Json.Dynamic/Extensions/Utf8JsonWriterExts.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace Alba.Text.Json.Dynamic.Extensions;
+
+public static class Utf8JsonWriterExts
+{
+    extension(Utf8JsonWriter @this)
+    {
+        public void WriteValue(object? value)
+        {
+            var node = value.ToJsonNode();
+            if (node == null)
+                @this.WriteNullValue();
+            else
+                node.WriteTo(@this);
+        }
+    }
+}
